82dee1b0e1dfa6dc55e3a0cdcf7345a9957941f7
ToniPCM <apcmdesarrolloweb@gmail.com>
2026-04-15 15:38:52 +0200

    Modificado app.routes.server.ts: añadido RenderMode.Server para el path: 'pokemon/:id'

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemon/:id',
+    renderMode: RenderMode.Server, // Esto fuerza el SSR para cada petición
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Prerender, // El resto se puede pre-renderizar
   },
 ];
